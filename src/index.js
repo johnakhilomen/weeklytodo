d374146962b6452180b0734f99bb301d748ca37d
John Akhilomen <jonbasanti@gmail.com>
2020-09-04 18:45:21 -0500

    Added new function for rendering index page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import $ from "jQuery";
+
 export let init = () => {
     console.log("Called from init");
+    renderIndexPage();
+}
+
+let renderIndexPage = () => { //function renderIndexPage
+    $("#root").html("Hi there");
 }
