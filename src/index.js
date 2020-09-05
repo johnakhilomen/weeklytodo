21e633d67753fdc3fe901b31705d21729a42a927
John Akhilomen <jonbasanti@gmail.com>
2020-09-05 11:38:51 -0500

    Refactored some styles

--- src/index.js
+++ src/index.js
@@ -18,9 +18,27 @@ let generateContainer = () => {
     ${headerComponent()}
     <div class="container">
     <h5>Start your weekly planner</h5>
-    <br>
     ${generateForm()}
     </div>`;
 }
 
+let getNumberOfDays = (month, year) => {
+    return new Date(year, month, 0).getDate(); 
+}
+ 
+let createCalendar = () => {
+    //Jan starts from 0 and December ends at 11 for months
+var firstDay = new Date(2020, 0, 1);
+var lastDay = new Date(2020, 0 + 1, 0);
 
+    let table = `
+    <table>
+    
+    </table>`
+}
+
+let tableRow = () => {
+    let trow = `<tr>
+    
+    </tr>`
+}
